b7eb2580626b16336e39d5d7fb2961c3d571f8f8
fernandoacupula <fernando.lima@acupula.com.br>
2018-09-18 16:01:25 -0300

    inicio dos estudos de javascrypt

---
+++ js/script.js
@@ -0,0 +1,58 @@
+function funcaoiframe() {
+    document.getElementById("siteacoplado").style.width = "1250px";
+    document.getElementById("siteacoplado").style.height = "500px";
+    document.getElementById("siteacoplado").style.border = "none";
+}		
+
+function mapa() {
+    var opcaomapa = {
+        center: new google.maps.LatLng(-23.1175, -46.5567),
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
+    }
+    var mapa= new google.maps.Map(document.getElementById("mapa"),opcaomapa);
+}
+    var x = document.getElementById("demo");
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+        
+    }   
+	else {
+        	x.innerHTML = "Geolocation is not supported by this browser.";
+	}
+}
+function showPosition(position) {
+		x.innerHTML = "Latitude: " + position.coords.latitude + 
+   		"<br>Longitude: " + position.coords.longitude; 
+}
+function showPosition(position) {
+    	var latlon = position.coords.latitude + "," + position.coords.longitude;
+    	var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
+    	+latlon+"&zoom=14&size=400x300&key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU";
+        document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
+        document.getElementById("btnMapa").style.visibility="hidden";
+            }
+            function allowDrop(ev) {
+                ev.preventDefault();
+         }	
+
+         function drag(ev) {
+            
+                 ev.dataTransfer.setData("text", ev.target.id);
+         }
+
+         function drop(ev) {
+             ev.preventDefault();
+             var data = ev.dataTransfer.getData("text");
+             ev.target.appendChild(document.getElementById(data));
+             
+    }  
+      
+    function calculadora(x,y) {
+        var x = document.getElementById("Calc1");
+        var y = document.getElementById("Calc2");
+        //var z = document.getElementById("operacao");
+        document.getElementById("resultado").innerHTML = x+y;
+
+         }
